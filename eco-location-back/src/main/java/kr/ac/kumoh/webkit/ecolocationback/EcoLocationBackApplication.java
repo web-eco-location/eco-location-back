15b2c32f0586744ad4073e7abb194b95c8596501
Jeonwoojae <dnwo0719@icloud.com>
2023-05-04 16:58:37 +0900

    [feat]: spring 프로젝트 생성

---
+++ eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/EcoLocationBackApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.webkit.ecolocationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoLocationBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoLocationBackApplication.class, args);
+	}
+
+}
---
+++ eco-location-back/src/test/java/kr/ac/kumoh/webkit/ecolocationback/EcoLocationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.webkit.ecolocationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoLocationBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

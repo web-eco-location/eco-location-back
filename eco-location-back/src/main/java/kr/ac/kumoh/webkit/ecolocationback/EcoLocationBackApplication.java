6bc1ee587ee4e94846434c41c1ed0cdb54dfb656
MK5532 <121079522+MK5532@users.noreply.github.com>
2023-05-27 16:00:17 +0900

    [feat] : 스케줄러를 이용해 지정된 시간에 데이터베이스를 초기화, csv 파일을 다시 읽어 저장하는 기능

--- eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/EcoLocationBackApplication.java
+++ eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/EcoLocationBackApplication.java
@@ -3,10 +3,11 @@ package kr.ac.kumoh.webkit.ecolocationback;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
-
+@EnableScheduling
 public class EcoLocationBackApplication {
 
 	public static void main(String[] args) {

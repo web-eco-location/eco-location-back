742313fdd19be74a6d17cc8adde0263a4316c98b
Jeonwoojae <dnwo0719@icloud.com>
2023-05-28 16:56:59 +0900

    [feat]: es 연결 주소 설정

--- eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/config/ElasticSearchConfig.java
+++ eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/config/ElasticSearchConfig.java
@@ -10,6 +10,7 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.ElasticsearchClient;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -19,11 +20,13 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackageClasses = {AreaGeneratorSourceSearchRepository.class, EnergyPotentialSearchRepository.class, GeneratorSearchRepository.class})
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+    @Value("${elasticsearch.hostAndPort}")
+    private String esHostAndPort;
 
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(esHostAndPort)
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }

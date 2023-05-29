872206bd69303fc89d2290386fe244b85f5afaa8
Jeonwoojae <dnwo0719@icloud.com>
2023-05-29 15:47:37 +0900

    [fix]: es 쿼리로 수정

--- eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/repository/AreaGeneratorSourceSearchRepository.java
+++ eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/repository/AreaGeneratorSourceSearchRepository.java
@@ -2,14 +2,19 @@ package kr.ac.kumoh.webkit.ecolocationback.repository;
 
 import kr.ac.kumoh.webkit.ecolocationback.entity.AreaGeneratorSource;
 import kr.ac.kumoh.webkit.ecolocationback.entity.document.AreaGeneratorSourceDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface AreaGeneratorSourceSearchRepository extends ElasticsearchRepository<AreaGeneratorSourceDocument, Long> {
+public interface AreaGeneratorSourceSearchRepository extends ElasticsearchRepository<AreaGeneratorSourceDocument, String> {
+    @Query("{\"bool\": {\"must\": [{\"match\": {\"area\": \"?0\"}}]}}")
     Iterable<AreaGeneratorSourceDocument> findByArea(String area);
+    @Query("{\"bool\": {\"must\": [{\"match\": {\"date\": \"?0\"}}]}}")
     Iterable<AreaGeneratorSourceDocument> findByDate(LocalDate Date);
+    @Query("{\"bool\": {\"must\": [{\"range\": {\"date\": {\"gte\": \"?0\", \"lte\": \"?1\"}}}]}}")
     Iterable<AreaGeneratorSourceDocument> findByDateBetween(LocalDate StartDate, LocalDate EndDate);
+    @Query("{\"bool\": {\"must\": [{\"match\": {\"date\": \"?0\"}, {\"range\": {\"date\": {\"gte\": \"?1\", \"lte\": \"?2\"}}]}}")
     Iterable<AreaGeneratorSourceDocument> findByAreaAndDateBetween(String Area, LocalDate StartDate, LocalDate EndDate);
 }
--- eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/repository/GeneratorSearchRepository.java
+++ eco-location-back/src/main/java/kr/ac/kumoh/webkit/ecolocationback/repository/GeneratorSearchRepository.java
@@ -2,14 +2,17 @@ package kr.ac.kumoh.webkit.ecolocationback.repository;
 
 import kr.ac.kumoh.webkit.ecolocationback.entity.Generator;
 import kr.ac.kumoh.webkit.ecolocationback.entity.document.GeneratorDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface GeneratorSearchRepository extends ElasticsearchRepository<GeneratorDocument, Long> {
-	
+public interface GeneratorSearchRepository extends ElasticsearchRepository<GeneratorDocument, String> {
+
+	@Query("{\"bool\": {\"must\": [{\"match\": {\"detailArea\": \"?0\"}}]}}")
 	Iterable<GeneratorDocument> findByDetailArea(String detailArea);
 
+	@Query("{\"bool\": {\"must\": [{\"match\": {\"powerSource\": \"?0\"}}]}}")
 	Iterable<GeneratorDocument> findByPowerSource(String powerSource);
 }
